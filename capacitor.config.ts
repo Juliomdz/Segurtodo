8c61d155338ce29e0c2d74a806d9b4e5f3b96f2c
Juliomdz <julio.mdz@hotmail.com>
2024-09-26 22:12:19 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Segurtodo.app',
+  appName: 'Segurtodo',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
